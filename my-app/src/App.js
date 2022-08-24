805ea549d4d7fea8f9b70d8da9c5cb87036459ef
shiva moeini <shivamoeini2000@gmail.cpm>
2022-08-24 19:33:14 +0430

    makestyles for buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,35 +9,56 @@ import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import Checkbox from "@material-ui/core/CheckBox";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from "@material-ui/core/TextField";
+import {makeStyles}from "@material-ui/core/styles"
 
 
-function CheckboxExample(){
-  const [checked,setChecked]=React.useState(true);
-  return(
-   
+
+
+
+
+
+const useStyles=makeStyles({
+  root:{
+    background:'linear-gradient(45deg,#FE6888,#FF8E53)',
+    border:0,
+    borderRadius:10,
+    color:'white',
+    padding:'10 35px',
+    margin:15,
+  }
+})
+function ButtonStyled(){
+  const classes=useStyles();
+  return <Button className={classes.root}>Test Styles Button</Button>
+}
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+
     <FormControlLabel
-    control={<Checkbox checked={checked} onChange={(e)=>setChecked(e.target.checked)} />}
-    label="Secondary"
-  />      
-    
+      control={<Checkbox checked={checked} onChange={(e) => setChecked(e.target.checked)} />}
+      label="Secondary"
+    />
+
   )
 }
 function App() {
   return (
     <div className="App">
-      
+
       <Typography variant="h3">Welcome to material ui shiva</Typography>
-<ButtonGroup>
-      <Button variant="contained" color="primary" size="small" style={{fontSize:13}} startIcon={<LockIcon />} >
-        Login
-      </Button>
-      <Button variant="contained" color="secondary" size="small" style={{fontSize:13}}  endIcon={<PersonAddIcon />}>
-        Register
-      </Button>
+      <ButtonGroup>
+        <Button variant="contained" color="primary" size="small" style={{ fontSize: 13 }} startIcon={<LockIcon />} >
+          Login
+        </Button>
+        <Button variant="contained" color="secondary" size="small" style={{ fontSize: 13 }} endIcon={<PersonAddIcon />}>
+          Register
+        </Button>
       </ButtonGroup>
-<CheckboxExample/>
-<TextField variant="outlined" color="secondary" label="Enter Email" type="email" placeholder="test@email.com"/>
-{/* type="time" */}
+      <CheckboxExample />
+      <TextField variant="outlined" color="secondary" label="Enter Email" type="email" placeholder="test@email.com" />
+      {/* type="time" */}
+      <ButtonStyled/>
     </div>
   );
 }
