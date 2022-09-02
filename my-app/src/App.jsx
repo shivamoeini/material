a3c90a961eba9994d64809d01e0cd739c038bfae
shiva moeini <shivamoeini2000@gmail.cpm>
2022-09-02 11:42:42 +0430

    fix-error-home

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import Home from './components/Home';
 function App() {
   return (
-  <Router>
-    <Route path='/'>
-      <Home />
+    <BrowserRouter>
+    
+    
+  <Routes>
+    <Route path='/' element={ <Home />}>
+     
     </Route>
-  </Router>
+  </Routes>
+    </BrowserRouter>
   )
 }
 
